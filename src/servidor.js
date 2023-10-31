2b6c32d1985976c5f64d36afb02e476a3995598f
Enel Sotillo <enelsotillo@gmail.com>
2023-10-31 19:20:04 -0300

    creando rutas o servicios servidor

---
+++ src/servidor.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const usuarioRouter = require('./routes/usuarioRoutes.js');
+//const
+const app = express();
+const PORT = 3000;
+
+app.use(usuarioRouter);
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
